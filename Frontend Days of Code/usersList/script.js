f777e7d18af01f03f0edef0d4758301316229839
Meekaylah <kaylahusanga@gmail.com>
2023-12-21 22:44:29 +0100

    Fixed tab selection bug

--- Frontend Days of Code/usersList/script.js
+++ Frontend Days of Code/usersList/script.js
@@ -22,23 +22,32 @@ fetch("./data.json")
 )
 
 const tags = document.querySelectorAll(".tag");
+let selected = null;
 
 function addStyle(tag) {
     tag.style.color = 'rgb(255, 255, 255)';
     tag.style.backgroundColor = 'rgb(132, 159, 255)';
+    selected = tag;
 }
 
 function removeStyle(tag) {
     tag.style.color = 'rgb(0, 0, 0)';
     tag.style.backgroundColor = 'rgb(255, 255, 255)';
+    selected = null;
 }
 
 tags.forEach(tag => {
     tag.addEventListener("click", () => {
         const color = window.getComputedStyle(tag).getPropertyValue("background-color");
-
-        if (color === "rgb(132, 159, 255)") {
+        
+        if(color === "rgb(132, 159, 255)") {
             removeStyle(tag);
+        } else if (selected!==null) {
+            const selectedColor = window.getComputedStyle(selected).getPropertyValue("background-color");
+            if (selectedColor === "rgb(132, 159, 255)") {
+                removeStyle(selected);
+                addStyle(tag);
+            }
         } else {
             addStyle(tag);
         }
